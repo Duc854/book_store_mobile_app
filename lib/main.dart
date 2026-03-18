6e4e121a566d1624c7672ae03f529b0a1ab5139c
trieuhoangson <sonthhe186811@fpt.edu.vn>
2026-03-18 13:10:55 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/screens/admin_books_screen.dart';
-import 'package:test_project/screens/admin_category_screen.dart';
+import 'package:book_store_mobile_app/screens/admin_books_screen.dart';
+import 'package:book_store_mobile_app/screens/admin_category_screen.dart';
 
 import 'themes/app_theme.dart';
 import 'screens/admin_dashboard.dart';
