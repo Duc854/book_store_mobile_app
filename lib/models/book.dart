60e01c1971d7e9a4a91663d177ca1e5b778323e1
trieuhoangson <sonthhe186811@fpt.edu.vn>
2026-03-24 14:01:03 +0700

    Update book.dart

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -6,6 +6,10 @@ class Book {
   final String description;
   final double price;
   final double rating;
+  final bool isBestSeller;
+  final int soldCount;
+  final int stock;
+  final int categoryId;
 
   Book({
     required this.id,
@@ -15,21 +19,43 @@ class Book {
     required this.description,
     required this.price,
     required this.rating,
+    required this.isBestSeller,
+    required this.soldCount,
+    required this.stock,
+    required this.categoryId,
   });
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(
       id: json['id'] is int ? json['id'] : int.tryParse('${json['id']}') ?? 0,
+
       title: json['title']?.toString() ?? 'No title',
       author: json['author']?.toString() ?? 'Unknown',
       imageUrl: json['imageUrl']?.toString() ?? '',
       description: json['description']?.toString() ?? '',
+
       price: (json['price'] is num)
           ? (json['price'] as num).toDouble()
           : double.tryParse('${json['price']}') ?? 0.0,
+
       rating: (json['rating'] is num)
           ? (json['rating'] as num).toDouble()
           : double.tryParse('${json['rating']}') ?? 0.0,
+
+      isBestSeller: json['isBestSeller'] == true ||
+          json['isBestSeller']?.toString().toLowerCase() == 'true',
+
+      soldCount: json['soldCount'] is int
+          ? json['soldCount']
+          : int.tryParse('${json['soldCount']}') ?? 0,
+
+      stock: json['stock'] is int
+          ? json['stock']
+          : int.tryParse('${json['stock']}') ?? 0,
+
+      categoryId: json['categoryId'] is int
+          ? json['categoryId']
+          : int.tryParse('${json['categoryId']}') ?? 0,
     );
   }
 }
