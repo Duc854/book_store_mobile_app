7546bd7cf5b1f1bea4da9a9e7918a069629a806d
trieuhoangson <sonthhe186811@fpt.edu.vn>
2026-03-24 15:01:49 +0700

    updateAdmin

--- lib/services/admin_dashboard_service.dart
+++ lib/services/admin_dashboard_service.dart
@@ -1,16 +1,34 @@
 import 'dart:convert';
+import 'package:book_store_mobile_app/services/auth_service.dart';
 import 'package:http/http.dart' as http;
 
 class AdminDashboardService {
   static const String baseUrl = "https://localhost:7128/api/admin";
 
+  // Lấy headers có JWT
+  static Future<Map<String, String>> _getHeaders() async {
+    final token = await AuthService().getToken();
+    return {
+      "Content-Type": "application/json",
+      if (token.isNotEmpty) "Authorization": "Bearer $token",
+    };
+  }
+
+  // Lấy thống kê
   static Future<Map<String, dynamic>> getStats() async {
-    final res = await http.get(Uri.parse("$baseUrl/stats"));
+    final headers = await _getHeaders();
+
+    final res = await http.get(
+      Uri.parse("$baseUrl/stats"),
+      headers: headers, // 🔥 thêm header
+    );
 
     if (res.statusCode == 200) {
       return jsonDecode(res.body);
+    } else if (res.statusCode == 401) {
+      throw Exception("Unauthorized: token missing or expired");
     } else {
-      throw Exception("Load stats failed");
+      throw Exception("Load stats failed: ${res.statusCode}");
     }
   }
 }
